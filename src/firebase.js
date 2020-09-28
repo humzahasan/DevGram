38ed6e97fbdb2dddb327dbb7b8fa3f072355426b
bravopy <humzahasan0126@gmail.com>
2020-09-28 12:22:05 +0530

    Deployed on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCLMam2FXHa_Sc_iBgmsWUF-hBndZyEcBQ',
-  authDomain: 'devgram-6e3ab.firebaseapp.com',
-  databaseURL: 'https://devgram-6e3ab.firebaseio.com',
-  projectId: 'devgram-6e3ab',
-  storageBucket: 'devgram-6e3ab.appspot.com',
-  messagingSenderId: '16911598653',
-  appId: '1:16911598653:web:f15973b3c8749c72171654',
-  measurementId: 'G-88LR92LS9Z',
+  apiKey: 'AIzaSyD0Bbbh53kQPgZEqau25O1ViVuN_RTl4_M',
+  authDomain: 'devgrampro.firebaseapp.com',
+  databaseURL: 'https://devgrampro.firebaseio.com',
+  projectId: 'devgrampro',
+  storageBucket: 'devgrampro.appspot.com',
+  messagingSenderId: '775754837391',
+  appId: '1:775754837391:web:74c35f59a213c9b8f82ffa',
+  measurementId: 'G-44S42N7FJL',
 });
 
 const db = firebaseApp.firestore();
